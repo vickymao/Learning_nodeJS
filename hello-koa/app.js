e92b2b29a934b1468ded44df421de6d3ec524b1f
MAO Yuan (INTERN) <YMO2@dsone.3ds.com>
2017-08-07 16:59:56 +0200

    KOA2

---
+++ hello-koa/app.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const Koa = require('koa');
+const router = require('koa-router')();
+const app = new Koa();
+const bodyParser = require('koa-bodyparser');
+// app.use((ctx, next) => {
+
+//     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+// });
+router.get('/hello/:name', (ctx, next) => {
+    var name = ctx.params.name;
+    ctx.response.type = 'text/html';
+    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+});
+router.get('/', (ctx, next) => {
+    ctx.response.type = 'text/html';
+    ctx.response.body = "<h1>Index</h1><form action='/signin' method='POST'><p>Name: <input name='name' value='koa '></p><p>Password: <input name='password' type='password'></p><p><input type='submit' value='Submit'></p></form>";
+});
+router.post('/signin', (ctx, next) => {
+    var
+        name = ctx.request.body.name || '',
+        password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+    if (name === 'koa' && password === '12345') {
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    } else {
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    }
+});
+app.use(bodyParser());
+app.use(router.routes());
+
+app.listen(3000);
+console.log('app started at port 3000 ...');
--- hello/main.js
+++ hello/main.js
@@ -1,7 +1,14 @@
 'use strict';
 
 var coucou = require('./hello');
-
+var http = require('http');
+var server = http.createServer(function(request, response) {
+    console.log(request.method + ': ' + request.url);
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end('<h1>Hello world!</h1>');
+});
+server.listen(8080);
+console.log('Server is running at http://localhost:8080/');
 var s = 'vicky';
 coucou.greet(s);
 coucou.call(s);
